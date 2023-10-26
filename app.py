99ed42913001d0945412ae71689e4be885a05518
fanisvl <fanis.vlahogiannis@gmail.com>
2023-10-26 21:09:07 +0300

    display upcoming from db

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from database import get_upcoming
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    upcoming_movies = upcoming()
+    return render_template("index.html", movies=upcoming_movies)
+
+
+@app.route("/api/upcoming")
+def upcoming():
+    return get_upcoming()
