48d8c1082edaa40f036bd6c3e65ecffb55c23545
fanisvl <fanis.vlahogiannis@gmail.com>
2023-10-28 23:18:03 +0300

    fixed movie not found bug

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ def home():
 
 @app.route('/movie/<int:movie_id>')
 def movie_page(movie_id):
-    try: 
-        movie_data = get_movie(movie_id)
-    except:
+
+    movie_data = get_movie(movie_id)    
+    if movie_data:
+        return render_template('movie.html', movie_data=movie_data)
+    else:
         return "404: Movie not found"
-    
-    return render_template('movie.html', movie_data=movie_data)
 
 @app.route("/api/upcoming")
 def upcoming():
