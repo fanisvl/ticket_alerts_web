8f8e8e1404a2bd13e7d79b0f9859fe941ae269cb
fanisvl <fanis.vlahogiannis@gmail.com>
2023-10-29 19:56:06 +0200

    form submission works

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from database import get_upcoming, get_movie, post_alert
+import json
 
 app = Flask(__name__)
 
@@ -21,9 +22,9 @@ def movie_page(movie_id):
 def upcoming():
     return get_upcoming()
 
-@app.route('/create_alert', methods=['POST'])
+@app.route('/alert_created', methods=['POST'])
 def create_alert():
     email = request.form.get('email')
-    movie_data = request.form.get('movie_data')
+    movie_data = json.loads(request.form.get('movie_data').replace("'", '"')) # replace sigle quotes with double quotes for valid json
     post_alert(email, movie_data['id'])
-    return render_template("alert_success", movie_data=movie_data, email=email)
+    return render_template("alert_created.html", movie_data=movie_data, email=email)
