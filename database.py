b7a620835993d2cc714c7f84b168585f6d7e600e
fanisvl <fanis.vlahogiannis@gmail.com>
2023-11-05 10:07:37 +0200

    use with statement to isolate db connections

--- database.py
+++ database.py
@@ -3,24 +3,27 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-db = mysql.connector.connect(
+
+def get_db_connection():
+    return mysql.connector.connect(
     host=os.getenv("DB_HOST"),
     user=os.getenv("DB_USERNAME"),
     password=os.getenv("DB_PASSWORD"),
     database=os.getenv("DB_NAME")
 )
 
-cursor = db.cursor(buffered=True, dictionary=True)
-
 def get_upcoming():
-    cursor.execute("SELECT * FROM upcoming_movies")
-    return cursor.fetchall()
+    with get_db_connection() as db, db.cursor(buffered=True, dictionary=True) as cursor:
+        cursor.execute("SELECT * FROM upcoming_movies")
+        return cursor.fetchall()
 
 def get_movie(id):
-    cursor.execute(f"SELECT * FROM upcoming_movies WHERE id={id}")
-    return cursor.fetchone()
+    with get_db_connection() as db, db.cursor(buffered=True, dictionary=True) as cursor:
+        cursor.execute(f"SELECT * FROM upcoming_movies WHERE id={id}")
+        return cursor.fetchone()
 
 def post_alert(email, id):
-    insert_query =  "INSERT INTO alerts (email, movie_id) VALUES (%s, %s)"
-    cursor.execute(insert_query, (email, id))
-    db.commit()
+    with get_db_connection() as db, db.cursor(buffered=True, dictionary=True) as cursor:
+        insert_query =  "INSERT INTO alerts (email, movie_id) VALUES (%s, %s)"
+        cursor.execute(insert_query, (email, id))
+        db.commit()
